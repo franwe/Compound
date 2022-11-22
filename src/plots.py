8d29eed7b65da7956393d705740dd3e0bc3248c7
franwe <franziska.wehrmann@gmail.com>
2022-11-22 16:17:03 +0200

    :building_construction: move `plot_returns_histogram`

--- src/plots.py
+++ src/plots.py
@@ -9,3 +9,11 @@ def plot_mc_paths(symbol, mc):
     ax.set_ylabel("Price [USD]")
     fig.suptitle(f"Simulated Price Paths - {symbol}")
     return fig
+
+
+def plot_returns_histogram(returns, N, bins=50):
+    fig = plt.hist(returns, bins=bins)
+    plt.xlabel("Returns")
+    plt.ylabel("Amount")
+    plt.title(f"Simulated Portfolio Returns (N={N})")
+    return fig
